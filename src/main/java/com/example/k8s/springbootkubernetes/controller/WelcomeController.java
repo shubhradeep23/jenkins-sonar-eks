c8dbcc6a4788434fbbb3485f37d9efcdd10266f7
shubhradeep23 <devops.april.2022@gmail.com>
2022-04-09 21:56:54 +0530

    minor change: image version

--- src/main/java/com/example/k8s/springbootkubernetes/controller/WelcomeController.java
+++ src/main/java/com/example/k8s/springbootkubernetes/controller/WelcomeController.java
@@ -8,7 +8,7 @@ public class WelcomeController {
 	
 	@RequestMapping("/greeting")
 	public String greeting() {
-		return "Welcome to Shubhradeeps Channel";
+		return "Welcome to Diptos Channel";
 	}
 
 }
