f294c8c047f2becff23e6a0ad215e2e5239642cf
shubhradeep23 <devops.april.2022@gmail.com>
2022-04-11 22:14:15 +0530

    Build, Test & Deploy Java App through Jenkins Declarative Pipeline on AWS EKS

--- src/main/java/com/example/k8s/springbootkubernetes/controller/WelcomeController.java
+++ src/main/java/com/example/k8s/springbootkubernetes/controller/WelcomeController.java
@@ -8,7 +8,7 @@ public class WelcomeController {
 	
 	@RequestMapping("/greeting")
 	public String greeting() {
-		return "Welcome to Shubhradeep's Channel ";
+		return "<h1>Welcome to Shubhradeep's Channel</h1>";
 	}
 
 }
