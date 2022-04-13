c0a0bbe2b102119430d736c0d2d75a0ae869b3c6
shubhradeep23 <devops.april.2022@gmail.com>
2022-04-13 12:40:17 +0530

    Deploy Java App on AWS EKS

--- src/main/java/com/example/k8s/springbootkubernetes/controller/WelcomeController.java
+++ src/main/java/com/example/k8s/springbootkubernetes/controller/WelcomeController.java
@@ -8,7 +8,7 @@ public class WelcomeController {
 	
 	@RequestMapping("/greeting")
 	public String greeting() {
-		return "Deploy Java App to AWS EKS";
+		return "Deploy Java App to AWS EKS by Shubhradeep";
 	}
 
 }
